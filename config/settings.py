04e7eb9512fbc566dc5843e1d49e106d78e3008f
nurbekomar <omarnurbek13@gmail.com>
2025-11-23 16:45:54 +0500

    add home apps

--- config/settings.py
+++ config/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG", default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", ".localhost", "0.0.0.0"]
 
 
 # Application definition
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "apps.home",
 ]
 
 MIDDLEWARE = [
